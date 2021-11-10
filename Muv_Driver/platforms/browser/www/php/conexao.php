b1581f289ad3556fb3512e5f3f86b2d1076d2abf
lealalves <leealalves@hotmail.com>
2021-11-10 18:25:00 -0300

    Pequena alteração foto usuario ao aceitar corrida

--- Muv_Driver/platforms/browser/www/php/conexao.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-header("Access-Control-Allow-Origin:*");
-$con=mysqli_connect("localhost","admin","admin","projeto")or die("Não pode conectar");
-
-?>
--- Muv_Driver/platforms/browser/www/php/confirmarCorrida.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'conexao.php';
-
-session_start();
-
-$query = mysqli_query($con, "update corrida set status='aceita', infDriver=". $_SESSION['codDriver'] . " where infUser =" . $_SESSION['clientId']);
